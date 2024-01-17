1f4431e02274eef07c2e9820e1f6133d7f94eb27
[RosieOh] <[20172207@gm.hannam.ac.kr]>
2024-01-18 03:53:46 +0900

    🏬build : 주문관련 API 프로젝트 빌드🏬

---
+++ src/main/java/com/memberorderapi/MemberorderapiApplication.java
@@ -0,0 +1,13 @@
+package com.memberorderapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MemberorderapiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MemberorderapiApplication.class, args);
+    }
+
+}
